6286acef644fd0306ac66a2192c85c1c3f804701
Walter.Kimbrough <walterkimbrough@AV-MBP-7432.local>
2019-04-24 10:34:11 -0700

    all sortsa stuff

--- project/models.py
+++ project/models.py
@@ -3,6 +3,28 @@
 from project import db
 from datetime import datetime
 
+class Project(db.Model):
+    __tablename__ = "projects"
+
+    project_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    priority = db.Column(db.Integer, nullable=False)
+    posted_date = db.Column(db.Date,default=datetime.utcnow())
+    status = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __init__(self, name, due_date, priority, posted_date, status, user_id):
+        self.name = name
+        self.due_date = due_date
+        self.priority = priority
+        self.posted_date = posted_date
+        self.status = status
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<name {0}>'.format(self.name)
+
 class Task(db.Model):
     __tablename__ = "tasks"
 
